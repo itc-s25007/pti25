bd93f2a97434a9cb4343de04eecd28098faddd2b
itc-s25007/WebExtention <s25007@std.it-college.ac.jp>
2025-07-07 16:02:42 +0900

    おみくじアプリ作成

---
+++ omikuji_App.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import random
+def omikuji():
+    kuji = ["大吉","中吉","吉","小吉","凶","大凶"]
+    rate = [0,150,400,550,950,1000,1100]
+    r = random.randint(1,1100)
+    return [kuji[i] for i in range(0,7) if r > rate[i] and r <= rate[i+1]]
+
+
+
+def displabel():
+    lbl.configure(text = omikuji())
+
+root = tk.Tk()
+root.geometry("200x100")
+
+lbl = tk.Label(text="LABEL")
+btn = tk.Button(text="おみくじ", command = displabel)
+
+lbl.pack()
+btn.pack()
+tk.mainloop()
